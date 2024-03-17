0131d1f9c17ee3c36c913d51db45f6300276d704
Joselyn <joselynserranof@gmail.com>
2024-03-17 14:25:23 -0400

    new project

---
+++ src/components/home/second-page.data.ts
@@ -0,0 +1,16 @@
+import type { secondPage } from '@/interfaces/secondPage'
+
+export const data: Array<secondPage> = [
+  {
+    id: 1,
+    cover: '/images/Pic2.png',
+  },
+  {
+    id: 2,
+    cover: '/images/Pic3.png',
+  },
+  {
+    id: 3,
+    cover: '/images/Pic4.png',
+  }
+]
